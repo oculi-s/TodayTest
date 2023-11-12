3536477f1ba1b4f307148a62b6092c74ffd293c5
oculis <oculis0925@yonsei.ac.kr>
2023-11-12 22:21:58 +0900

    20231112_222100

--- android/app/bin/src/main/kotlin/com/example/recommendation/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.recommendation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/todaytest/package/name/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.todaytest.package.name
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
